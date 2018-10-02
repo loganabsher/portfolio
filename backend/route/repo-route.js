53aaa8b419cf63d9995f9a381a8979dd3990a472
Logan Absher <absherlogan@gmail.com>
2018-10-02 12:43:35 -0700

    seting up staging branch

--- backend/route/repo-route.js
+++ backend/route/repo-route.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const Router = require('express').Router;
+
+const repoRouter = exports = module.exports = Router();
+
+repoRouter.post('/api/repositories/')
+
+repoRouter.get('/api/repositories', (req, res, next) => {
+
+});
