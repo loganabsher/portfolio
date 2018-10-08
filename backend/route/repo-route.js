227c5cac3be69489ffb569d18eaa1c1972e61c80
Logan Absher <absherlogan@gmail.com>
2018-10-08 12:38:46 -0700

    adding new routes, adding documentation while testing them with httpie

--- backend/route/repo-route.js
+++ backend/route/repo-route.js
@@ -14,22 +14,21 @@ const loader = function () {
     .set({'Authorization': 'token ' + process.env.GITHUB_TOKEN})
     .end((req, res) => {
       res.body.forEach((ele) => {
-        Repository.findOne({'name': ele.name})
+        Repository.findOne({name: ele.name})
           .then((repo) => {
             if(!repo) {
               new Repository(ele).save();
               return;
             }
             if(repo.updated_at !== ele.updated_at) {
-              console.log(ele.name);
-              console.log(ele.updated_at);
-              console.log(repo.updated_at);
               console.log('changes noticed');
-              let newRepo = new Repository(ele);
-              Repository.replaceOne({'name': ele.name}, newRepo)
-              .then((res) => {
-                console.log(res);
-              })
+              console.log(ele.name, '--', ele.updated_at);
+              console.log(repo.name, '--', repo.updated_at);
+              let newRepo = {name: ele.name, size: ele.size, created_at: ele.created_at, updated_at: ele.updated_at};
+              Repository.findOneAndUpdate({name: ele.name}, newRepo)
+                .then((res) => {
+                  console.log(res);
+                });
             }
           });
       });
@@ -38,12 +37,18 @@ const loader = function () {
 
 repositoryRoute.get('/api/repositories/:id', (req, res, next) => {
   Repository.findById(req.params.id)
-  .then((repo) => {
-    res.json(repo);
-  })
-  .catch(next)
+    .then((repo) => {
+      res.json(repo);
+    })
+    .catch(next);
 });
 
-// repositoryRoute.get('/api/repositories', )
+repositoryRoute.get('/api/repositories/all', (req, res, next) => {
+  Repository.find({})
+    .then((repos) => {
+      res.json(repos);
+    })
+    .catch(next);
+});
 //
 // repositoryRoute.get('/api/repositories/branches', )
