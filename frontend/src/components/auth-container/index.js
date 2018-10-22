7375b5f89fa1d50d0b6c206c5ffcea1cd2871ac8
Logan Absher <absherlogan@gmail.com>
2018-10-22 12:06:55 -0700

    working on comtainer dispactches and actions

--- frontend/src/components/auth-container/index.js
+++ frontend/src/components/auth-container/index.js
@@ -4,18 +4,40 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
+import {signupRequest, loginRequest} from '../../../actions/auth-actions.js';
+import LoginForm from '../forms/login-form.js';
+
 class AuthContainer extends React.Component{
   constructor(props){
     super(props);
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(e){
+
   }
 
   render(){
     return(
       <div className='auth-container'>
         <p>forms go here</p>
+        <LoginForm />
       </div>
-    )
+    );
   }
 }
 
-export default AuthContainer;
+let mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    signup: (user) => dispatch(signupRequest(user)),
+    login: (user) => dispatch(loginRequest(user)),
+  };
+};
+
+export default (mapStateToProps, mapDispatchToProps)(AuthContainer);
