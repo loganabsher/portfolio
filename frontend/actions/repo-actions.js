8fb833072ea20db9d1cae6d2543aa4f6b324706d
Logan Absher <absherlogan@gmail.com>
2018-10-22 12:18:14 -0700

    auth reducer fixed

--- frontend/actions/repo-actions.js
+++ frontend/actions/repo-actions.js
@@ -7,16 +7,14 @@ import superagent from 'superagent';
 export const repoSet = (repo) => ({
   type: 'REPOS_SET',
   payload: repo
-})
+});
 
 export const allRepositoriesRequest = () => (dispatch) => {
-  return superagent.get(`${API_URL}/api/repositories/all`)
+  return superagent.get(`${process.env.API_URL}/api/repositories/all`)
     .then((res) => {
       console.log('finding repos', res);
       dispatch(repoSet(res.body));
       return res.body;
     })
-    .catch((err) => {
-      console.error(err);
-    })
-}
+    .catch((err) => console.error(err));
+};
