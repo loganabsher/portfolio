bd69e8bb4d891af5754306e33b9b800a79c0235e
Logan Absher <absherlogan@gmail.com>
2019-01-24 12:18:55 -0800

    fixed issue with user_set and token_set in the frontend

--- backend/route/user-router.js
+++ backend/route/user-router.js
@@ -8,7 +8,6 @@ const Promise = require('bluebird');
 const createError = require('http-errors');
 
 const basicAuth = require('../lib/basic-auth-middleware.js');
-// const bearerAuth = require('../lib/bearer-auth-middleware.js');
 const User = require('../model/User.js');
 const Profile = require('../model/Profile.js');
 const Message = require('../model/Message.js');
@@ -24,6 +23,10 @@ userRouter.post('/api/signup', jsonParser, (req, res, next) => {
 
   User.findOne({email: req.body.email})
     .then((user) => {
+      // NOTE: this is just temporary until I create something to authenticate their email
+      // NOTE: this is interesting, because if they are already authenticated with google, facebook
+      // or twitter, then what will happen when they try to sign up normally, I think I need to add
+      // some sort of catch for if they are already authenticated
       if(user){
         if(user.authenticated){
           // NOTE: maybe update all error codes and texts to be very specific
@@ -31,8 +34,7 @@ userRouter.post('/api/signup', jsonParser, (req, res, next) => {
         }else{
           user.generatePasswordHash('normal', password)
             .then((user) => user.generateToken())
-            .then((token) => res.json(token))
-            .catch(next);
+            .then((token) => res.json(token));
         }
       }
       else{
@@ -47,8 +49,7 @@ userRouter.post('/api/signup', jsonParser, (req, res, next) => {
 
         user.generatePasswordHash('normal', password)
           .then((user) => user.generateToken())
-          .then((token) => res.json(token))
-          .catch(next);
+          .then((token) => res.json(token));
       }
     })
     .catch(next);
@@ -62,23 +63,45 @@ userRouter.get('/api/login', basicAuth, (req, res, next) => {
       if(!user) return Promise.reject(createError(401, 'user not found'));
       return user.comparePasswordHash('normal', req.auth.password);
     })
-    .then((user) => user.generateToken())
-    .then((token) => {
-      res.cookie('portfolio-login-token', token, {maxAge: 900000000});
-      res.json(token);
+    .then((user) => {
+      user.generateToken()
+        .then((token) => {
+          let cookieOptions = {maxAge: 900000000};
+          res.cookie('portfolio-login-token', token, cookieOptions);
+          // NOTE: this needs to be removed for production, changed return to token
+          res.json(token);
+        });
     })
     .catch(next);
 });
 
+userRouter.get('/api/userexisits/:id', (req, res, next) => {
+  debug('GET: /api/userexisits/:id');
+
+  User.findById(req.params.id)
+    .then((user) => {
+      if(user) res.status(204).send();
+      else{
+        res.status(404).send();
+      }
+    })
+    .catch(next);
+});
+
+// NOTE: this should really have some sort of authentication
+userRouter.get('/api/allaccounts', (req, res, next) => {
+  debug('GET: /api/allaccounts');
+
+  User.find({})
+    .then((users) => res.json(users))
+    .catch(next);
+});
+
 // NOTE: need to change from findbyid to find by email/user
-// NOTE: this also probably needs email auth
-// NOTE: should maybe also add a way to email reset password
-userRouter.put('/api/changepassword', basicAuth, jsonParser, (req, res, next) => {
-  debug('PUT: /api/changepassword');
+userRouter.put('/api/updatepassword/:id', basicAuth, jsonParser, (req, res, next) => {
+  debug('PUT: /api/updatepassword/:id');
 
-  if(!req.body || !req.body.password) return next(createError(400, 'password must be provided'));
-
-  User.findOne({'email': req.auth.email})
+  User.findById(req.params.id)
     .then((user) => {
       if(!user) return Promise.reject(createError(404, 'not found'));
       return user.comparePasswordHash('normal', req.auth.password);
@@ -95,27 +118,25 @@ userRouter.put('/api/changepassword', basicAuth, jsonParser, (req, res, next) =>
 });
 
 // NOTE: should probably add some sort of wait method that waits a few weeks before actually deleting the account and rather just have it be disabled, kinda like what facebook does
-userRouter.delete('/api/deleteaccount', basicAuth, (req, res, next) => {
-  debug('DELETE: /api/deleteaccount');
-  // NOTE: I'm trying to decide if bearerAuth would be a good idea in this and the update password querys
+userRouter.delete('/api/deleteaccount/:id', basicAuth, (req, res, next) => {
+  debug('DELETE: /api/deleteaccount/:id');
 
-  User.findOne({email: req.auth.email})
+  let id = {'_id': req.params.id};
+  User.findById(id)
     .then((user) => {
       if(!user) return Promise.reject(createError(404, 'not found'));
       return user.comparePasswordHash('normal', req.auth.password);
     })
     .then((user) => {
-      if(user.profileId) Profile.deleteOne({'_id': user.profdileId});
+      if(user.profileId) Profile.deleteOne({'_id': user.profileId});
       return user;
     })
     .then((user) => {
       Message.deleteMany({authorId: user.id});
-      return user;
     })
-    .then((user) => {
-      User.findByIdAndRemove({'_id': user._id})
-        .then(() => res.status(204).send())
-        .catch(next);
+    .then(() => {
+      User.deleteOne(id)
+        .then(() => res.status(204).send());
     })
     .catch(next);
 });
